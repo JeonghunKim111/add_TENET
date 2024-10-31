b486e6bb57d8a0d88e2c5ffbdcaa23069398cf8b
JeonghunKim111 <gs13022@gmail.com>
2024-10-31 16:02:27 +0900

    os2_px8_fault_remove

--- data/mapping/conv2d_os2_ox32_8x8.m
+++ data/mapping/conv2d_os2_ox32_8x8.m
@@ -1,2 +1,2 @@
 {S[k,c,ox,oy,rx,ry]->PE[(ox+32*oy)%8,k%8]}
-{S[k,c,ox,oy,rx,ry]->T[floor((ox+32*oy)/16),floor(k/16),floor((ox+32*oy)/16)+floor(k/16),c]}
+{S[k,c,ox,oy,rx,ry]->T[floor((ox+32*oy)/8),floor(k/8),floor((ox+32*oy)/8)+floor(k/8),c]}
